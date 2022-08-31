90b01a5373df78e700e110677c06440d10976c71
Bruno Alves de Araujo <88730176+brunodev21@users.noreply.github.com>
2022-08-31 16:55:11 -0300

    primeiro commit

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,10 @@
-//Crie a const para a frase aqui
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
+
+const novaString = frase.replace("verde", "rosa").replace("azul", "laranja")
+
+const stringVerde = novaString.includes("verde")
+console.log("Contem verde? "+stringVerde)
+
+const stringLaranja = novaString.includes("laranja")
+console.log("Contem laranja? "+stringLaranja)
+
